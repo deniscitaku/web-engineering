a1196691592c397dcebd272c0f6b2cad7d0dce69
d.citaku <d.citaku@celonis.com>
2020-05-24 19:55:30 +0200

    Completed first phase

--- final-project-zoo/js/form-validation.js
+++ final-project-zoo/js/form-validation.js
@@ -1,27 +1,50 @@
-// TODO add validation
+const validateName = (id, message = "Invalid name") => {
+    const input = getInputById(id);
+    const value = input.value;
+    console.log('Value: ', value);
 
-/*
-const validateName = (value) => {
-    if (value.length > 5) {
-        return false;
+    const isValidName = /^[A-Z]{1}[a-z]{2,20}[ ]{1}[A-Z]{1}[a-z]{2,20}$/.test(value);
+    console.log("Is valid name: ", isValidName)
+    showErrorSpan(id, isValidName, message);
+}
+
+const validateEmail = (id, message = "Invalid email") => {
+    const input = getInputById(id);
+    const value = input.value;
+
+    const isValidEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(value);
+    showErrorSpan(id, isValidEmail, message);
+}
+
+
+const validateLength = (id, min, max, message = `Length must be between ${min} and ${max}`) => {
+    const input = getInputById(id);
+    const value = input.value;
+
+    const length = value.length;
+    showErrorSpan(id, length > min && length < max, message);
+}
+
+const validatePassword = (id, message = `Password must contain upper case, lower case and special character`) => {
+    const input = getInputById(id);
+    const value = input.value;
+
+    const isValidRegex = /((?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%]).+)/.test(value);
+    showErrorSpan(id, isValidRegex, message);
+}
+
+const showErrorSpan = (id, isValid, message) => {
+    let errorSpan = document.getElementById(id).getElementsByClassName("error")[0];
+    console.log("ShowErrorSpan: ", isValid, message);
+    if (isValid) {
+        errorSpan.style.visibility = "hidden"; // Hides the error
+    } else {
+        errorSpan.textContent = message;
+        errorSpan.style.visibility = "visible"; // Displays the error
     }
-
-    return true;
 }
 
-const validateEmail = (value) => {
-    return true;
+const getInputById = (id) => {
+    let input = document.getElementById(id).getElementsByTagName("input");
+    return input && input.length > 0 ? input[0] : document.getElementById(id).getElementsByTagName("textarea")[0];
 }
-
-
-const validateLength = (input, min, max) => {
-
-    let length = input.value.length;
-    return length > min && length < max;
-}
-
-const getErrorSpan = (input) => {
-    let errorSpan;
-    while ((errorSpan = input.nextSibling.getElementsByClassName('error')[0]));
-    return errorSpan;
-}*/
